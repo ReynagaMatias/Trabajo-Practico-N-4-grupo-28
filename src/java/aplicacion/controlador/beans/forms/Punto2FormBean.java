8429f967b7752f0b2a657fc5e28d881e3756fcd0
Alejandro Reynaga <reynaga2807@gmail.com>
2018-04-24 23:39:54 -0300

    se agrego el paquete punto2formbean con la clase puntoformbean y el gestorarreglo y el indexpunto2

---
+++ src/java/aplicacion/controlador/beans/forms/Punto2FormBean.java
@@ -0,0 +1,38 @@
+package aplicacion.controlador.beans.forms;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import java.io.Serializable;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+import punto2.dominio.GestorArreglo;
+
+/**
+ *
+ * @author Alejandro
+ */
+@ManagedBean
+@RequestScoped
+public class Punto2FormBean implements Serializable{
+    GestorArreglo gestor = new GestorArreglo();
+
+    /**
+     * Creates a new instance of Punto2FormBean
+     */
+    public Punto2FormBean() {
+        gestor = new GestorArreglo();
+    }
+
+    public GestorArreglo getGestor() {
+        return gestor;
+    }
+
+    public void setGestor(GestorArreglo gestor) {
+        this.gestor = gestor;
+    }
+    
+}
