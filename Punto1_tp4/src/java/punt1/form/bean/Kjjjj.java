b4ae1d25a4232772471e867cedf0d5ef1cd45e2d
InesCoraite <ribeiro@LAPTOP-OQ6UM8CQ>
2018-05-06 01:41:31 -0300

    se modifico el punto 7

---
+++ Punto1_tp4/src/java/punt1/form/bean/Kjjjj.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package punt1.form.bean;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+
+/**
+ *
+ * @author ribeiro
+ */
+@ManagedBean
+@RequestScoped
+public class Kjjjj {
+    
+    private double numroConvertir;
+    private String tipoConversor;
+
+    /**
+     * Creates a new instance of Kjjjj
+     */
+    public Kjjjj() {
+    }
+    
+    public void conversion(){
+        switch (tipoConversor){
+            case Far:
+                return
+                        
+        }
+                
+    }
+    
+}
